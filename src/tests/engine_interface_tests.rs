a21e88382a3444b3dd3f7aa7828cfb89acee2957
macher259 <mh418267@students.mimuw.edu.pl>
2021-04-13 00:58:30 +0200

    Added tests for searching events by their duration.

--- src/tests/engine_interface_tests.rs
+++ src/tests/engine_interface_tests.rs
@@ -3,7 +3,8 @@ use planner::engine;
 use planner::engine::db_wrapper::event::Event;
 use planner::engine::db_wrapper::user::delete_user_from_database;
 use planner::engine::db_wrapper::{user, Connection};
-use planner::engine::{add_event, delete_event};
+use planner::engine::{add_event, delete_event, delete_user};
+use sqlx::postgres::types::PgInterval;
 
 #[test]
 fn find_events_with_name_like_wszy() {
@@ -46,4 +47,72 @@ fn find_event_by_date() {
 
     delete_event(pool, &added).unwrap();
     block_on(delete_user_from_database(pool, &user)).unwrap();
+}
+
+#[test]
+fn find_event_by_duration() {
+    let connection = Connection::new();
+    let pool = &connection.unwrap().pool;
+    let user = user::User::new()
+        .username("duaration test")
+        .password("Nananannana");
+    let added = block_on(user::insert_user(pool, &user));
+
+    assert!(added);
+
+    let event = Event::new()
+        .date(chrono::Utc::now() + chrono::Duration::days(12))
+        .duration(PgInterval {
+            months: 21,
+            days: 3,
+            microseconds: 7,
+        });
+
+    let min = PgInterval {
+        months: 0,
+        days: 0,
+        microseconds: 0,
+    };
+    let max = PgInterval {
+        months: 30,
+        days: 0,
+        microseconds: 0,
+    };
+    let bad_max = PgInterval {
+        months: 20,
+        days: 0,
+        microseconds: 0,
+    };
+    let bad_min = PgInterval {
+        months: 28,
+        days: 0,
+        microseconds: 0,
+    };
+
+    add_event(pool, &user, &event).unwrap();
+
+    let criteria = engine::GetEventsCriteria::new().duration_between(min, max);
+    let events = engine::get_user_events_by_criteria(pool, &user, criteria);
+    assert_eq!(events.unwrap().len(), 1);
+
+    let max = PgInterval {
+        months: 30,
+        days: 0,
+        microseconds: 0,
+    };
+    let criteria = engine::GetEventsCriteria::new().duration_between(bad_min, max);
+    let events = engine::get_user_events_by_criteria(pool, &user, criteria);
+    assert_eq!(events.unwrap().len(), 0);
+
+    let min = PgInterval {
+        months: 0,
+        days: 0,
+        microseconds: 0,
+    };
+
+    let criteria = engine::GetEventsCriteria::new().duration_between(min, bad_max);
+    let events = engine::get_user_events_by_criteria(pool, &user, criteria);
+    assert_eq!(events.unwrap().len(), 0);
+
+    delete_user(pool, &user).unwrap();
 }
